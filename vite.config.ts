4537ea98157038e8d175237526133106a8f6cd5e
Joppe Koops <112714380+joppekoops@users.noreply.github.com>
2025-03-24 16:20:52 +0100

    Add github action for deploying to github pages (#7)
    Add github action for deploying to github pages
    
    - Add github workflow for deploying to github pages in a new subdirectory and add a link to the release
    - Change `vite.config.ts` to use relative base for github pages
    
    (This does not work with React router)

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import svgr from 'vite-plugin-svgr'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+    base: './',
     plugins: [
         react(),
         plainText(
