973e43302b391f7a5ad6aefc9456c0bb37a62bd5
joppekoops <joppe.koops@endeavour.nl>
2025-04-11 13:02:07 +0200

    Extract optionbar and togglebutton to separate components

--- src/components/Icon/set/index.ts
+++ src/components/Icon/set/index.ts
@@ -1,5 +1,17 @@
 import VideoFile from './VideoFile.svg'
+import AlignLeft from './AlignLeft.svg'
+import AlignCenter from './AlignCenter.svg'
+import AlignRight from './AlignRight.svg'
+import Bold from './Bold.svg'
+import Italics from './Italics.svg'
+import Underline from './Underline.svg'
 
 export const icons = {
     videoFile: VideoFile,
+    alignLeft: AlignLeft,
+    alignCenter: AlignCenter,
+    alignRight: AlignRight,
+    bold: Bold,
+    italics: Italics,
+    underline: Underline,
 }
